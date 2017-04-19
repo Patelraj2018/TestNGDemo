0f55f3deaaaa694b442affaa3ab7143c431ea1c6
rajpatel2018 <rajan.patel89@yahoo.com>
2017-04-19 17:35:21 -0400

    Today checkin

---
+++ TestNGDemo/src/Demo/TestCase.java
@@ -0,0 +1,24 @@
+package Demo;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestCase {
+	   
+	    @Test
+	    public void f() {
+	    	System.out.println("Raj");
+	    }
+	 
+	    @BeforeTest
+	    public void beforeTest() {
+	    	
+	    }
+	 
+	    @AfterTest
+	    public void afterTest() {
+	    	
+	    }
+
+}
